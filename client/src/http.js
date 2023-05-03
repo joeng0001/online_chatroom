933a65fff4999f45406e7b08d9f1d00fcdf6a578
joeng0001 <102034869+joeng0001@users.noreply.github.com>
2023-05-03 20:28:58 +0800

    remove client file

--- client/src/http.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-export default axios.create({
-  baseURL: "http://localhost:8089/",
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*"
-  }
-});
